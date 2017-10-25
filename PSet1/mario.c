c3b9f1323696b99f68fce4f50d31401d01ee9017
Raidas Grisk <30408368+RaidasGrisk@users.noreply.github.com>
2017-10-25 21:26:59 +0300

    Add files via upload

---
+++ PSet1/mario.c
@@ -0,0 +1,53 @@
+/* CS50:          https://cs50.harvard.edu/weeks
+*  Problem set 1: https://docs.cs50.net/2017/fall/psets/1/pset1.html
+*  Problem 1:     https://docs.cs50.net/problems/mario/more/mario.html
+*
+* The code asks user to specify pyramid height and then prints out mario-pyramid accordingly.
+*/
+
+#include <stdio.h>
+#include <cs50.c>
+
+int main(void){
+
+  // initialize variables
+  int height, spaces, hashes, row;
+
+  // prompt for integer
+  do
+  {
+    height = get_int("Let build a Mario pyramid! Buy how high? Give me a positive int between 0 and 24: ");
+  }
+  while (height < 0 || height > 24);
+
+  // loop through each row
+  for (row = 1; row <= height; row++)
+  {
+
+    // print spaces
+    for (spaces = height; spaces > row; spaces--)
+    {
+      printf(" ");
+    }
+
+    // print hashes (and one space in the middle)
+    for (hashes = 0; hashes <= row * 2; hashes++)
+    {
+      if (hashes == row)
+      {
+        printf(" ");
+      }
+      else
+      {
+        printf("#");
+      }
+    }
+
+    // jump to next line
+    printf("\n");
+
+  }
+
+  return 0;
+
+}
