08969493df6c8a267a6b9c49e8dfc4f8627dc413
Raidas Grisk <30408368+RaidasGrisk@users.noreply.github.com>
2017-11-03 18:29:59 +0200

    Add files via upload

---
+++ pset3/fifteen/fifteen.c
@@ -0,0 +1,281 @@
+/**
+ * fifteen.c
+ *
+ * Implements Game of Fifteen (generalized to d x d).
+ *
+ * Usage: fifteen d
+ *
+ * whereby the board's dimensions are to be d x d,
+ * where d must be in [DIM_MIN,DIM_MAX]
+ *
+ * Note that usleep is obsolete, but it offers more granularity than
+ * sleep and is simpler to use than nanosleep; `man usleep` for more.
+ */
+
+#define _XOPEN_SOURCE 500
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// constants
+#define DIM_MIN 3
+#define DIM_MAX 9
+
+// board
+int board[DIM_MAX][DIM_MAX];
+
+// dimensions
+int d;
+
+// prototypes
+void clear(void);
+void greet(void);
+void init(void);
+void draw(void);
+bool move(int tile);
+bool won(void);
+
+int main(int argc, string argv[])
+{
+    // ensure proper usage
+    if (argc != 2)
+    {
+        printf("Usage: fifteen d\n");
+        return 1;
+    }
+
+    // ensure valid dimensions
+    d = atoi(argv[1]);
+    if (d < DIM_MIN || d > DIM_MAX)
+    {
+        printf("Board must be between %i x %i and %i x %i, inclusive.\n",
+            DIM_MIN, DIM_MIN, DIM_MAX, DIM_MAX);
+        return 2;
+    }
+
+    // open log
+    FILE *file = fopen("log.txt", "w");
+    if (file == NULL)
+    {
+        return 3;
+    }
+
+    // greet user with instructions
+    greet();
+
+    // initialize the board
+    init();
+
+    // accept moves until game is won
+    while (true)
+    {
+        // clear the screen
+        clear();
+
+        // draw the current state of the board
+        draw();
+
+        // log the current state of the board (for testing)
+        for (int i = 0; i < d; i++)
+        {
+            for (int j = 0; j < d; j++)
+            {
+                fprintf(file, "%i", board[i][j]);
+                if (j < d - 1)
+                {
+                    fprintf(file, "|");
+                }
+            }
+            fprintf(file, "\n");
+        }
+        fflush(file);
+
+        // check for win
+        if (won())
+        {
+            printf("ftw!\n");
+            break;
+        }
+
+        // prompt for move
+        printf("Tile to move: ");
+        int tile = get_int();
+
+        // quit if user inputs 0 (for testing)
+        if (tile == 0)
+        {
+            break;
+        }
+
+        // log move (for testing)
+        fprintf(file, "%i\n", tile);
+        fflush(file);
+
+        // move if possible, else report illegality
+        if (!move(tile))
+        {
+            printf("\nIllegal move.\n");
+            usleep(500000);
+        }
+
+        // sleep thread for animation's sake
+        usleep(500000);
+    }
+
+    // close log
+    fclose(file);
+
+    // success
+    return 0;
+}
+
+/**
+ * Clears screen using ANSI escape sequences.
+ */
+void clear(void)
+{
+    printf("\033[2J");
+    printf("\033[%d;%dH", 0, 0);
+}
+
+/**
+ * Greets player.
+ */
+void greet(void)
+{
+    clear();
+    printf("WELCOME TO GAME OF FIFTEEN\n");
+    usleep(2000000);
+}
+
+/**
+ * Initializes the game's board with tiles numbered 1 through d*d - 1
+ * (i.e., fills 2D array with values but does not actually print them).
+ */
+void init(void)
+{
+    // get starting value
+    int value = d * d - 1;
+
+    // fill the board with values
+    for (int i = 0; i < d; i++)
+    {
+        for (int j = 0; j < d; j++)
+        {
+            board[i][j] = value--;
+        }
+    }
+
+    // swap 1 and 2 if dim is even
+    if (d % 2 == 0)
+    {
+        board[d-1][d-3] = 1;
+        board[d-1][d-2] = 2;
+    }
+}
+
+/**
+ * Prints the board in its current state.
+ */
+void draw(void)
+{
+    // draw board
+    for (int i = 0; i < d; i++)
+    {
+        for (int j = 0; j < d; j++)
+        {
+            if (board[i][j] == 0)
+            {
+                printf(" _ ");
+            }
+            else
+            {
+                printf("%2i ", board[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+/**
+ * If tile borders empty space, moves tile and returns true, else
+ * returns false.
+ */
+bool move(int tile)
+{
+    // find tile and underscore
+    int t_row;
+    int t_col;
+    int u_row;
+    int u_col;
+
+    for (int i = 0; i < d; i++)
+    {
+        for (int j = 0; j < d; j++)
+        {
+            // check if tile is found
+            if (board[i][j] == tile)
+            {
+                t_row = i;
+                t_col = j;
+            }
+
+            // check if underscore is found
+            if (board[i][j] == 0)
+            {
+                u_row = i;
+                u_col = j;
+            }
+        }
+    }
+
+    // swap values
+    if ((t_row + 1 == u_row && t_col == u_col) ||
+        (t_row - 1 == u_row && t_col == u_col) ||
+        (t_col + 1 == u_col && t_row == u_row) ||
+        (t_col - 1 == u_col && t_row == u_row))
+    {
+        int temp_row = board[t_row][t_col];
+        board[t_row][t_col] = board[u_row][u_col];
+        board[u_row][u_col] = temp_row;
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+}
+
+/**
+ * Returns true if game is won (i.e., board is in winning configuration),
+ * else false.
+ */
+bool won(void)
+{
+    int iter = 1;
+
+    for (int i = 0; i < d; i++)
+    {
+        for (int j = 0; j < d; j++)
+        {
+            // check if order is accending
+            if (board[i][j] != iter)
+            {
+                return false;
+            }
+            iter++;
+
+            // check if we hit last tile
+            if (iter == d * d)
+            {
+                break;
+            }
+        }
+    }
+
+    return true;
+}
---
+++ pset3/find/helpers.c
@@ -0,0 +1,119 @@
+/**
+ * helpers.c
+ *
+ * Helper functions for Problem Set 3.
+ */
+
+#include <cs50.h>
+#include "helpers.h"
+
+// binary search algorithm
+bool binary_search(int value, int values[], int n)
+{
+    // mark first and last index of the list
+    int first = 0;
+    int last = n - 1;
+
+    // binary search
+    while (first <= last)
+    {
+        int middle = (last + first) / 2;
+
+        if (values[middle] == value)
+        {
+            return true;
+        }
+        else if (values[middle] > value)
+        {
+            last = middle - 1;
+        }
+        else if(values[middle] < value)
+        {
+            first = middle + 1;
+        }
+    }
+
+    return false;
+}
+
+// selection sort algorithmn
+void selection_sort(int values[], int n)
+{
+
+    // temporary int for swapping
+    int temp;
+
+    // loop through each value from the beggining
+    for (int i = 0; i < n; i++)
+    {
+
+        // compare the value with the rest of values in front
+        int i_min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (values[i_min] > values[j])
+            {
+                i_min = j;
+            }
+        }
+
+        // swap
+        if (i != i_min)
+        {
+            temp = values[i_min];
+            values[i_min] = values[i];
+            values[i] = temp;
+        }
+    }
+}
+
+// bubble sort algorithm
+void bubble_sort(int values[], int n)
+{
+
+    // temporary int for swapping and bool for sorted/not sorted
+    int temp;
+    bool sorted = false;
+
+    while(sorted != true)
+    {
+        sorted = true;
+        for (int i = 0; i < n-1; i++)
+        {
+            if (values[i] > values[i+1])
+            {
+                temp = values[i+1];
+                values[i+1] = values[i];
+                values[i] = temp;
+                sorted = false;
+            }
+        }
+    }
+}
+
+
+/**
+ * Returns true if value is in array of n values, else false.
+ */
+bool search(int value, int values[], int n)
+{
+    if (n < 0)
+    {
+        return false;
+    }
+    else
+    {
+        return binary_search(value, values, n);
+    }
+}
+
+/**
+ * Sorts array of n values.
+ */
+void sort(int values[], int n)
+{
+    selection_sort(values, n);
+    return;
+}
+
+
